fa72e953c102955af0973e6b1a07e9ef7fe756e9
arkamaprdxn <arkama@prdxn.com>
2020-08-05 15:11:31 +0530

    Updates

---
+++ precache-manifest.7e41d43b06b918721f158683784ee64c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d3a370324673e2f0a197f463ef1c4a8",
+    "url": "/redux-movie/index.html"
+  },
+  {
+    "revision": "1703ac31105d0f5938d0",
+    "url": "/redux-movie/static/css/main.25c94668.chunk.css"
+  },
+  {
+    "revision": "79377c75d5d20a30cafa",
+    "url": "/redux-movie/static/js/2.094d6e6b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/redux-movie/static/js/2.094d6e6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1703ac31105d0f5938d0",
+    "url": "/redux-movie/static/js/main.1ab1ad1e.chunk.js"
+  },
+  {
+    "revision": "157f1d3908d17c84de51",
+    "url": "/redux-movie/static/js/runtime-main.0c367f55.js"
+  }
+]);
