ae6eb10deecc0aa609e59cd8cb8e828fa03eca9f
arkamaprdxn <arkama@prdxn.com>
2020-08-05 14:49:02 +0530

    Updates

---
+++ precache-manifest.36612a7065a378e5b94b23a6273fb312.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccfafb29e2e7a8f388123064972105cb",
+    "url": "/aarkamprdxn/redux-movie/index.html"
+  },
+  {
+    "revision": "1703ac31105d0f5938d0",
+    "url": "/aarkamprdxn/redux-movie/static/css/main.25c94668.chunk.css"
+  },
+  {
+    "revision": "79377c75d5d20a30cafa",
+    "url": "/aarkamprdxn/redux-movie/static/js/2.094d6e6b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/aarkamprdxn/redux-movie/static/js/2.094d6e6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1703ac31105d0f5938d0",
+    "url": "/aarkamprdxn/redux-movie/static/js/main.1ab1ad1e.chunk.js"
+  },
+  {
+    "revision": "22f9b2fbdfbddccb15a0",
+    "url": "/aarkamprdxn/redux-movie/static/js/runtime-main.50e10096.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/aarkamprdxn/redux-movie.git/precache-manifest.9054e83f4b8c72443097ab555048f44d.js"
+  "/aarkamprdxn/redux-movie/precache-manifest.36612a7065a378e5b94b23a6273fb312.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/aarkamprdxn/redux-movie.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/aarkamprdxn/redux-movie/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
